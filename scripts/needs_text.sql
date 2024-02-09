db362dea350fd58492bf833d4e747bd3784fc72c
Forest Gregg <fgregg@datamade.us>
2024-02-09 17:01:59 -0500

    initial commit

---
+++ scripts/needs_text.sql
@@ -0,0 +1,17 @@
+COPY (
+    SELECT
+        media_type,
+        url
+    FROM
+        opencivicdata_billversionlink
+        INNER JOIN opencivicdata_billversion ON opencivicdata_billversion.id = version_id
+        INNER JOIN opencivicdata_bill ON opencivicdata_bill.id = bill_id
+        INNER JOIN opencivicdata_legislativesession ON opencivicdata_legislativesession.id = legislative_session_id
+    WHERE
+        opencivicdata_legislativesession.identifier = '2023'
+        AND opencivicdata_bill.extras -> 'key_legislation' = 'true'
+        AND text = '')
+TO STDOUT WITH CSV HEADER;
+
+
+
